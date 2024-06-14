c856b8ff1a6e22f069332ea8ad43bd32cf512bd1
Bilalkhan678 <bkhan2678@gmail.com>
2024-06-14 12:49:16 +0500

    Added

--- src/app/(web-layout)/about-us/page.jsx
+++ src/app/(web-layout)/about-us/page.jsx
@@ -1,10 +1,11 @@
 // import ServiceHistory from "app/(web-layout)/about-us/servicehistory";
 import ServiceHistory from './servicehistory/page';
+import styles from "../service.module.scss"
 
 const AboutUs = () => {
   return (
     <div>
-      {/* <h1>b</h1> */}
+      <h1 className={styles.heading}> Service History </h1>
       <ServiceHistory />
     </div>
   );
--- src/app/(web-layout)/about-us/servicehistory/page.jsx
+++ src/app/(web-layout)/about-us/servicehistory/page.jsx
@@ -3,13 +3,25 @@ import services from '../../../data/services';
 import styles from '../../service.module.scss';
 
 const ServiceCard = ({ service }) => {
+    let statusColor = '';
+    switch (service.status.toLowerCase()) {
+      case 'completed':
+        statusColor = '#4CAF50'; // Green color for Completed status
+        break;
+      case 'pending':
+        statusColor = '#FFC107'; // Yellow color for Pending status
+        break;
+      default:
+        statusColor = '#f9f9f9'; // Default background color
+        break;
+    }
   return (
     <Link href={`/servicesH/${service.id}`} legacyBehavior>
     <a className={styles.card}>
       <h2 className={styles.cardTitle}></h2>
-      <p className={styles.cardDetail}><strong></strong> {service.serviceName}</p>
-      <p className={styles.cardDetail}><strong></strong> {service.insuranceName}</p>
-      <p className={styles.cardDetail}><strong></strong> {service.status}</p>
+      <p className={styles.serviceName}><strong></strong> {service.serviceName}</p>
+      <p className={styles.insuranceName}><strong></strong> {service.insuranceName}</p>
+      <p className={styles.status}><strong></strong> {service.status}</p>
       {/* <Link href={`/servicesH/${service.id}`} legacyBehavior>
         <a className={styles.cardLink}>More Details</a>
       </Link> */}
